3cf083018f7cbf90002976c0ab1fb013434001db
Daniel da silva souza <82838453+DanielSouz4@users.noreply.github.com>
2021-08-25 16:10:55 -0300

    tratando rota que criar novo post e add no array

--- index.js
+++ index.js
@@ -8,31 +8,31 @@ let posts = [
     id: 'kdasj',
     title: 'Teste do Mural',
     description: 'Descrição teste'
-  },
-  {
-    id: 'kdasj',
-    title: 'Teste do Mural',
-    description: 'Descrição teste'
-  },
-  {
-    id: 'kdasj',
-    title: 'Teste do Mural',
-    description: 'Descrição teste'
   }
 ]
+
+// app.use('/', express.json()) ou app.use(express.json())
 
 app.get('/all', (req, res) => {
   res.json(JSON.stringify(posts))
 })
 
-app.post('/new', (req, res) => {})
+app.post('/new', express.json(), (req, res) => {
+  let id = generateID()
+  let title = req.body.title
+  let description = req.body.description
+
+  posts.push({ id, title, description })
+
+  res.send('Post adicionado')
+})
 
 app.listen(PORT, () => {
   console.log('Server running on port', PORT)
 })
 
-// faz server com express
-// fez duas rotas
-// 1 buscar all posts
-// 2 add novo post
-// fez array de posts para responder a rota /all
+function generateID() {
+  return Math.random().toString(36).substr(2, 9)
+}
+
+// tratar rota que criar um new post e add no array
