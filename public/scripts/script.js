59a3fcd0b9f373b060584b89b30c81a6a9b7b9f4
Daniel da silva souza <82838453+DanielSouz4@users.noreply.github.com>
2021-08-30 17:37:03 -0300

    cors

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 function updatePosts() {
-  fetch('http://localhost:3000/api/all')
+  fetch('http://192.168.0.107:3000/api/all')
     .then(res => {
       return res.json()
     })
@@ -41,7 +41,7 @@ function newPost() {
     body: JSON.stringify(post)
   }
 
-  fetch('http://localhost:3000/api/new', options).then(res => {
+  fetch('http://192.168.0.107:3000/api/new', options).then(res => {
     console.log(res)
     updatePosts()
     document.getElementById('title').value = ''
