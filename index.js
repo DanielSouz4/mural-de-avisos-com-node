7d739476cb46b41dbab3a4cba7ca665600c90bc4
Daniel da silva souza <82838453+DanielSouz4@users.noreply.github.com>
2021-08-30 11:13:20 -0300

    fazendo front que se conecta com back

--- index.js
+++ index.js
@@ -6,18 +6,10 @@ const path = require('path')
 const app = express()
 
 app.use('/', express.static(path.join(__dirname, 'public')))
-// se na pasta "public" tiver uma "all" ele retornará ela e não passará pra proxima require(que é uma require get "all")
-//resolver => criar rota garantir que não tem nada que bata com a rota all
-// 1) colar todas requires nessa rota
 app.use('/api', apiRoute)
 
 app.listen(PORT, () => {
   console.log('Server running on port', PORT)
 })
 
-// separando o projeto em rotas
-// antes criar pasta publica (p/ enviar arq estáticos)
-//
-
-// para garantir que uma chamada a api não busque por uma pasta api, devemos inverter a chamada das rotas
-// (app.use("/api", apsia) essa linha deve ser chamada primeiro)
+// fazendo front que se conecta com back
